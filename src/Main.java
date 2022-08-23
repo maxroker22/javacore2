45561dcd3cab352999687481eba4444c5cc87004
Максим Орлов <maxroker22@gmail.com>
2022-08-23 20:01:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.sort(Comparator.naturalOrder());
+
+            System.out.println(intList);
+        }
+    }
+
